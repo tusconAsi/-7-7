8bb5a6577f74a6b40fc894f43e0f9f4c9293ab65
tusconAsi <160646470+tusconAsi@users.noreply.github.com>
2024-09-03 12:54:11 +0300

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,45 +1,215 @@
-﻿#include <iostream> 
-#include <random> 
-using namespace std;
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <memory>
+#include <map>
 
-int main()
-{
-	setlocale(LC_ALL, "Rus");
+class Command {
+public:
+    virtual ~Command() {}
+    virtual void execute() = 0;
+    virtual void undo() = 0;
+};
 
-	random_device rand;
+class Clipboard {
+public:
+    std::string content;
+};
 
-	int const a = 30, b = 100, c = 500;
-	int mimi[a];
-	int min, max;
+class TextProcessor {
+private:
+    std::string text;
+public:
+    void setText(const std::string& newText) {
+        text = newText;
+    }
 
-	for (int i = 0; i < a; ++i) {
-		mimi[i] = b + rand() % (c - b);
-		cout << mimi[i] << " " << endl;
-	}
-	min = mimi[0];
-	max = mimi[0];
+    const std::string& getText() const {
+        return text;
+    }
 
-	for (int i = 1; i < a; i++) {
+    void copy(size_t index1, size_t index2, Clipboard& clipboard) {
+        clipboard.content = text.substr(index1, index2 - index1);
+    }
 
-		if (mimi[i] > max) {
-			max = mimi[i];
-		}
-		if (mimi[i] < min) {
-			min = mimi[i];
-		}
+    void paste(size_t index, const Clipboard& clipboard) {
+        text.insert(index, clipboard.content);
+    }
 
-	}
-	cout << min << "Минимальное" << endl << max << "Максимальное" << endl;
-	return 0;
+    void insert(const std::string& str, size_t index) {
+        text.insert(index, str);
+    }
+
+    void remove(size_t index1, size_t index2) {
+        text.erase(index1, index2 - index1);
+    }
+
+    void show() const {
+        std::cout << text << std::endl;
+    }
+};
+
+class CopyCommand : public Command {
+private:
+    TextProcessor& processor;
+    Clipboard& clipboard;
+    size_t idx1, idx2;
+public:
+    CopyCommand(TextProcessor& p, Clipboard& c, size_t i1, size_t i2)
+        : processor(p), clipboard(c), idx1(i1), idx2(i2) {}
+
+    void execute() override {
+        processor.copy(idx1, idx2, clipboard);
+    }
+
+    void undo() override {
+       
+    }
+};
+
+class PasteCommand : public Command {
+private:
+    TextProcessor& processor;
+    Clipboard& clipboard;
+    size_t idx;
+public:
+    PasteCommand(TextProcessor& p, Clipboard& c, size_t i)
+        : processor(p), clipboard(c), idx(i) {}
+
+    void execute() override {
+        processor.paste(idx, clipboard);
+    }
+
+    void undo() override {
+        
+    }
+};
+
+class InsertCommand : public Command {
+private:
+    TextProcessor& processor;
+    size_t idx;
+    std::string str;
+public:
+    InsertCommand(TextProcessor& p, const std::string& s, size_t i)
+        : processor(p), idx(i), str(s) {}
+
+    void execute() override {
+        processor.insert(str, idx);
+    }
+
+    void undo() override {
+        processor.remove(idx, idx + str.length());
+    }
+};
+
+class DeleteCommand : public Command {
+private:
+    TextProcessor& processor;
+    size_t idx1, idx2;
+    std::string removedText;
+public:
+    DeleteCommand(TextProcessor& p, size_t i1, size_t i2)
+        : processor(p), idx1(i1), idx2(i2) {}
+
+    void execute() override {
+        removedText = processor.getText().substr(idx1, idx2 - idx1);
+        processor.remove(idx1, idx2);
+    }
+
+    void undo() override {
+        processor.insert(removedText, idx1);
+    }
+};
+
+class CommandManager {
+private:
+    std::vector<std::shared_ptr<Command>> commandHistory;
+    size_t historyIndex = 0;
+public:
+    void executeCommand(std::shared_ptr<Command> command) {
+        command->execute();
+        if (historyIndex < commandHistory.size()) {
+            commandHistory.erase(commandHistory.begin() + historyIndex, commandHistory.end());
+        }
+        commandHistory.push_back(command);
+        historyIndex++;
+    }
+
+    void undo() {
+        if (historyIndex > 0) {
+            commandHistory[--historyIndex]->undo();
+        }
+    }
+
+    void redo() {
+        if (historyIndex < commandHistory.size()) {
+            commandHistory[historyIndex++]->execute();
+        }
+    }
+};
+
+class CommandParser {
+public:
+    void parseCommands(const std::string& filename,
+        TextProcessor& processor,
+        CommandManager& manager,
+        Clipboard& clipboard) {
+        std::ifstream infile(filename);
+        std::string line;
+        while (std::getline(infile, line)) {
+            std::istringstream iss(line);
+            std::string cmd;
+            iss >> cmd;
+
+            if (cmd == "copy") {
+                size_t idx1, idx2;
+                char comma; 
+                iss >> idx1 >> comma >> idx2;
+                manager.executeCommand(std::make_shared<CopyCommand>(processor, clipboard, idx1, idx2));
+            }
+            else if (cmd == "paste") {
+                size_t idx;
+                iss >> idx;
+                manager.executeCommand(std::make_shared<PasteCommand>(processor, clipboard, idx));
+            }
+            else if (cmd == "insert") {
+                std::string str;
+                char quote;
+                iss >> quote >> str >> quote;
+                size_t idx;
+                iss >> idx;
+                manager.executeCommand(std::make_shared<InsertCommand>(processor, str, idx));
+            }
+            else if (cmd == "delete") {
+                size_t idx1, idx2;
+                char comma;
+                iss >> idx1 >> comma >> idx2;
+                manager.executeCommand(std::make_shared<DeleteCommand>(processor, idx1, idx2));
+            }
+            else if (cmd == "undo") {
+                manager.undo();
+            }
+            else if (cmd == "redo") {
+                manager.redo();
+            }
+        }
+    }
+};
+
+int main() {
+    TextProcessor processor;
+    processor.setText("Hello, World! This is a sample text."); 
+
+    Clipboard clipboard;
+    CommandManager commandManager;
+    CommandParser commandParser;
+
+    commandParser.parseCommands("commands.txt", processor, commandManager, clipboard);
+
+    processor.show();
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
